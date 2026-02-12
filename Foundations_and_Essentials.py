9b8c72d3453b68feb083d17d7608d5c235824cdc
invoicecoder <greaterhillsrecyclingsquad@gmail.com>
2026-02-12 17:05:54 -0500

    Add files via upload

---
+++ Foundations_and_Essentials.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request
+from datetime import datetime
+import random
+
+app = Flask(__name__)
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        student_name = request.form['student_name']
+        parent_name = request.form['parent_name']
+        tutor_name = request.form['tutor_name']
+        director_name = request.form['director_name']
+        director_email = request.form['director_email']
+        month = request.form['month']
+        a_fee = int(request.form.get('a_fee', 0) or 0)
+        s_fee = int(request.form.get('s_fee', 0) or 0)
+        f_fee = int(request.form.get('f_fee', 0) or 0)
+        t_fee = int(request.form.get('t_fee', 0) or 0)
+
+        total = a_fee + s_fee + f_fee + t_fee
+
+        date = datetime.now().strftime("%Y-%m-%d")
+
+        return render_template('invoice.html',
+                               student_name=student_name,
+                               parent_name=parent_name,
+                               tutor_name=tutor_name,
+                               director_name=director_name,
+                               director_email=director_email,
+                               month=month,
+                               a_fee=a_fee,
+                               s_fee=s_fee,
+                               f_fee=f_fee,
+                               t_fee=t_fee,
+                               date=date,
+                               total=total)
+
+
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
