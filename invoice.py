629ca6e216ae028b988aa1dc4482cf63039bf3c1
invoicecoder <greaterhillsrecyclingsquad@gmail.com>
2026-02-14 12:15:33 -0500

    Update invoice.py

--- invoice.py
+++ invoice.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from datetime import datetime
 import random
+import os
 
 app = Flask(__name__)
 
@@ -41,6 +42,7 @@ def index():
 
     return render_template('index.html')
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
