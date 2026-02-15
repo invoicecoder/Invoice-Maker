9de4bb314727ea32dc4b55dc182b5af128f2b5e7
invoicecoder <greaterhillsrecyclingsquad@gmail.com>
2026-02-15 14:54:43 -0500

    Update invoice.py

--- invoice.py
+++ invoice.py
@@ -4,6 +4,7 @@ import random
 import os
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 REAL_PASSWORD = os.environ.get("APP_PASSWORD")
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -62,6 +63,7 @@ def index():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
