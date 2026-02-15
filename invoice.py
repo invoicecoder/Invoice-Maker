f614cb11d01b963e31f63ed7762960ab4920e324
invoicecoder <greaterhillsrecyclingsquad@gmail.com>
2026-02-15 14:35:49 -0500

    Refactor invoice.py for improved readability

--- invoice.py
+++ invoice.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request
 from datetime import datetime
 import random
 import os
@@ -47,6 +47,7 @@ def index():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
+
 
 
 
