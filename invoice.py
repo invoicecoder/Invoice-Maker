72e1bae372da2604eec11caacaa35efce3ebd5bd
invoicecoder <greaterhillsrecyclingsquad@gmail.com>
2026-02-16 10:54:21 -0500

    Configure session to be non-permanent
    Added session configuration to prevent session permanence.

--- invoice.py
+++ invoice.py
@@ -5,6 +5,7 @@ import os
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
+app.config['SESSION_PERMANENT'] = False
 REAL_PASSWORD = os.environ.get("APP_PASSWORD")
 
 @app.route("/health")
@@ -68,6 +69,7 @@ def index():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
